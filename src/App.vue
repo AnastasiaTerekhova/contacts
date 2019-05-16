60c5d21c739e91702c9d162cd76b7dc30bba8cab
AnastasiaTerekhova <45146154+AnastasiaTerekhova@users.noreply.github.com>
2019-05-16 23:30:11 +0300

    Files

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- <Contacts msg=" "/> -->
+    <Settings />
+  </div>
+</template>
+
+<script>
+// import Contacts from './components/Contacts.vue'
+import Settings from './components/Settings.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Settings
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
